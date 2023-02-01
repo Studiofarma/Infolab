0b8fd64c342dda6396d85c6eb0eba7737049a15d
Lorenzo Rinaldi <lorenzo.rinaldi.job@gmail.com>
2023-02-01 16:21:58 +0100

    Prima versione funzionante della rest api (prende soltanto un messaggio). Spostato DBConfig dalla directory model a quella esterna.

--- src/main/java/com/cgm/infolab/ChatApiMessagesController.java
+++ src/main/java/com/cgm/infolab/ChatApiMessagesController.java
@@ -1,13 +1,23 @@
 package com.cgm.infolab;
 
 import com.cgm.infolab.model.ChatMessage;
+import com.cgm.infolab.model.MessageType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class ChatApiMessagesController {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     // Tutorial: https://www.baeldung.com/spring-controller-vs-restcontroller
     // Tutorial: https://www.lateralecloud.it/spring-boot-rest-controller/
@@ -17,13 +27,24 @@ public class ChatApiMessagesController {
     // Se volete provare uno strumento piu' avanzato per le chiamate all'API usate Postman https://www.postman.com/downloads/
     @GetMapping("/api/messages")
     public List<ChatMessage> getAllMessages() {
-        ChatMessage message = new ChatMessage();
-        message.setContent("ciao");
-        message.setSender("luca");
-        ChatMessage message2 = new ChatMessage();
-        message2.setContent("ciao2");
-        message2.setSender("luca2");
-        return List.of(message, message2);
+        String query = "SELECT * FROM infolab.chatmessages WHERE id = ?";
+        ChatMessage chatMessage = jdbcTemplate.queryForObject(
+                query, new Object[] {2}, new MessageRowMapper());
+
+        return List.of(chatMessage);
+    }
+
+    private class MessageRowMapper implements RowMapper<ChatMessage> {
+        @Override
+        public ChatMessage mapRow(ResultSet rs, int rowNum) throws SQLException {
+            ChatMessage chatMessage = new ChatMessage();
+
+            chatMessage.setSender(rs.getString("sender_id"));
+            chatMessage.setContent(rs.getString("content"));
+            chatMessage.setType(MessageType.CHAT);
+
+            return chatMessage;
+        }
     }
 
 }
