a6f388416a4ba77bf5089080d7c36fb35cdc321b
Lorenzo Rinaldi <lorenzo.rinaldi.job@gmail.com>
2023-02-02 16:27:11 +0100

    Creato evento per l'aggiunta di tutte le room al db in una classe apposita.

---
+++ src/main/java/com/cgm/infolab/model/RunAfterStartup.java
@@ -0,0 +1,30 @@
+package com.cgm.infolab.model;
+
+import com.cgm.infolab.DBManager;
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RunAfterStartup {
+
+    public static final String[] ROOMS = {"general"};
+    private DBManager dbManager;
+
+    public RunAfterStartup(DBManager dbManager) {
+        this.dbManager = dbManager;
+    }
+
+    /**
+     * Metodo che aggiunge tutte le stanze all'avvio dell'app.
+     */
+    @EventListener(ApplicationReadyEvent.class)
+    public void addAllRooms() {
+        for (String s :
+                ROOMS) {
+            // TODO: eventualmente sostituire con batch operation.
+            dbManager.addRoom(s);
+        }
+    }
+}
