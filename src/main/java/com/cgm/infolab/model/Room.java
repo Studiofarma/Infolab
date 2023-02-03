d82e31c3650bbecfb78985fcace4cb41f06f648a
Lorenzo Rinaldi <lorenzo.rinaldi.job@gmail.com>
2023-02-03 16:53:42 +0100

    Created Room and User classes.

---
+++ src/main/java/com/cgm/infolab/model/Room.java
@@ -0,0 +1,18 @@
+package com.cgm.infolab.model;
+
+public class Room {
+
+    private String name;
+
+    public Room(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
