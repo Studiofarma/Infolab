43c257f7dae85b97e4a266d3d0367c1cbeaa2c87
Lorenzo Rinaldi <lorenzo.rinaldi.job@gmail.com>
2023-02-03 17:28:51 +0100

    Completed ChatMessageRepository.

---
+++ src/main/java/com/cgm/infolab/db/ChatMessageRepository.java
@@ -0,0 +1,89 @@
+package com.cgm.infolab.db;
+
+import com.cgm.infolab.model.ChatMessage;
+import com.cgm.infolab.model.Room;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Timestamp;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class ChatMessageRepository {
+    private JdbcTemplate jdbcTemplate;
+    private DataSource dataSource;
+    private UserRepository userRepository;
+    private RoomRepository roomRepository;
+
+    private final Logger log = LoggerFactory.getLogger(ChatMessageRepository.class);
+
+    public ChatMessageRepository(JdbcTemplate jdbcTemplate, DataSource dataSource,
+                                 UserRepository userRepository, RoomRepository roomRepository) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.dataSource = dataSource;
+        this.userRepository = userRepository;
+        this.roomRepository = roomRepository;
+    }
+
+    /**
+     * Metodo che aggiunge un messaggio al database.
+     * @param message messaggio da salvare.
+     * @return chiave che è stata auto generata per il messaggio creato, oppure -1 se il messaggio inserito esisteva già.
+     */
+    public long add(ChatMessage message) {
+        SimpleJdbcInsert simpleJdbcInsert = new SimpleJdbcInsert(dataSource)
+                .withSchemaName("infolab")
+                .withTableName("chatmessages")
+                .usingGeneratedKeyColumns("id");
+
+        long senderId = userRepository.readId(message.getUserSender());
+
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("sender_id", senderId);
+        // TODO: trovare un modo per prendere la room in cui il messaggio arriva.
+        parameters.put("recipient_room_id", roomRepository.readId(new Room("general")));
+        parameters.put("sent_at", timestamp);
+        parameters.put("content", message.getContent());
+
+        try {
+            return (long)simpleJdbcInsert.executeAndReturnKey(parameters);
+        } catch (DuplicateKeyException e) {
+            log.info(String.format("Message %s already exists in the database.", message));
+        }
+        return -1;
+    }
+
+    /**
+     * Metodo che ritorna tutti i messaggi mandati in una room.
+     * @param room da cui prendere i messaggi
+     * @return lista di messaggi trovati. Ritorna null se non è stato trovato nessun messaggio.
+     */
+    public List<ChatMessage> readByRoom(Room room) {
+        long roomId = roomRepository.readId(room);
+
+        String query = "SELECT * FROM infolab.chatmessages WHERE recipient_room_id = ?";
+
+        try {
+            return jdbcTemplate.query(query, new Object[]{roomId}, (rs, rowNum) -> {
+                ChatMessage chatMessage = new ChatMessage();
+                chatMessage.setSender(rs.getString("sender_id"));
+                chatMessage.setContent(rs.getString("content"));
+                return chatMessage;
+            });
+        } catch (EmptyResultDataAccessException e) {
+            log.info(String.format("Non sono stati trovati messaggi nella room %s", room.getName()));
+        }
+
+        return null;
+    }
+}
