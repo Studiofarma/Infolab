715989473999f5a2ce10290f4db4d0684b43533f
Pinnel06 <daviderichini@gmail.com>
2023-02-09 17:01:42 +0100

    eliminata classe non utilizzata

--- src/frontend/pages/chat/search-chats.js
+++ src/frontend/pages/chat/search-chats.js
@@ -101,22 +101,6 @@ export class SearchChats extends LitElement {
       font-weight: lighter;
     }
 
-    .material-icons {
-      font-family: "Material Icons";
-      font-weight: normal;
-      font-style: normal;
-      font-size: 24px;
-      line-height: 1;
-      letter-spacing: normal;
-      text-transform: none;
-      display: inline-block;
-      white-space: nowrap;
-      word-wrap: normal;
-      direction: ltr;
-      -webkit-font-smoothing: antialiased;
-      cursor: pointer;
-    }
-
     .dropdown > div:hover {
       background: lightgray;
     }
