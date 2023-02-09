5d57c80890e491130e39920a42cca7917dd37470
MB <matteobrusa2003@gmail.com>
2023-02-09 17:21:20 +0100

    salvataggio lavoro icone

--- src/frontend/components/button-icon.js
+++ src/frontend/components/button-icon.js
@@ -1,9 +1,10 @@
 import { LitElement, html, css } from "lit";
+import { mdiEye } from '@mdi/js';
 
 export class ButtonIcon extends LitElement {
   static get properties() {
     return {
-      content: "",
+      mdiEye,
     };
   }
 
@@ -12,14 +13,15 @@ export class ButtonIcon extends LitElement {
       display: inline;
       align-items: center;
     }
+    
     #button {
       height: 100%;
       width: 100%;
       justify-content: center;
       align-items: center;
-      font-family: "Material Icons";
       font-weight: normal;
       font-style: normal;
+      font-family: "Material Icons";
       font-size: 24px;
       line-height: 1;
       letter-spacing: normal;
@@ -33,10 +35,15 @@ export class ButtonIcon extends LitElement {
       user-select: none;
       min-height: 35px;
       min-width: 35px;
+      
     }
 
+    
+
+   
+
     #button:hover {
-      background-color: rgba(0, 0, 0, 0.12);
+      background-color: rgb(0, 0, 255);
       border-radius: 50%;
     }
   `;
@@ -44,10 +51,12 @@ export class ButtonIcon extends LitElement {
   constructor() {
     super();
     this.content = "";
+    console.log(mdiEye);
   }
 
   render() {
-    return html` <div id="button">${this.content}</div> `;
+    
+    return html` <div >${mdiEye}</div> `;
   }
 }
 
