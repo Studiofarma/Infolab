dc834b0d4526bb04ad5a8dc4455b4d862dfc1356
Lorenzo Rinaldi <lorenzo.rinaldi.job@gmail.com>
2023-02-25 10:34:46 +0100

    Creato RoomApiController. Versione finta dell'api.

---
+++ src/main/java/com/cgm/infolab/controller/RoomApiController.java
@@ -0,0 +1,33 @@
+package com.cgm.infolab.controller;
+
+import com.cgm.infolab.db.repository.RoomRepository;
+import com.cgm.infolab.model.RoomDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RoomApiController {
+
+    private final RoomRepository roomRepository;
+
+    private final Logger log = LoggerFactory.getLogger(RoomApiController.class);
+
+    @Autowired
+    public RoomApiController(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
+
+    @GetMapping("api/rooms")
+    public List<RoomDto> getAllRooms(@RequestParam(required = false)LocalDateTime dateLimit) {
+        List<RoomDto> roomDtos = new ArrayList<>();
+        roomDtos.add(RoomDto.of("UwU"));
+
+        return roomDtos;
+    }
+}
