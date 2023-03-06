fa4b2f800ee7e036a2e9ee8ab51273633aa5452a
Lorenzo Rinaldi <lorenzo.rinaldi.job@gmail.com>
2023-03-06 16:08:55 +0100

    Aggiunta List di ChatMessageEntities in RoomEntity.

--- src/main/java/com/cgm/infolab/db/model/RoomEntity.java
+++ src/main/java/com/cgm/infolab/db/model/RoomEntity.java
@@ -2,17 +2,21 @@ package com.cgm.infolab.db.model;
 
 import com.cgm.infolab.db.ID;
 
+import java.util.List;
+
 public class RoomEntity {
     private long id;
     private String name;
+    private List<ChatMessageEntity> messages;
 
-    private RoomEntity(long id, String name) {
+    private RoomEntity(long id, String name, List<ChatMessageEntity> messages) {
         this.id = id;
         this.name = name;
+        this.messages = messages;
     }
 
     public static RoomEntity of(String name) {
-        return new RoomEntity(ID.None, name);
+        return new RoomEntity(ID.None, name, null);
     }
 
     public long getId() {
@@ -29,5 +33,13 @@ public class RoomEntity {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<ChatMessageEntity> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<ChatMessageEntity> messages) {
+        this.messages = messages;
     }
 }
