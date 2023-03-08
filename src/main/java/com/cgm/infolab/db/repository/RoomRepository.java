361aa89f4738c756389de0ce1017a61ebe0d5268
Lorenzo Rinaldi <lorenzo.rinaldi.job@gmail.com>
2023-03-08 19:15:46 +0100

    Implementato controllo con le query che ritornano una room singola.

--- src/main/java/com/cgm/infolab/db/repository/RoomRepository.java
+++ src/main/java/com/cgm/infolab/db/repository/RoomRepository.java
@@ -22,12 +22,18 @@ public class RoomRepository {
     private final ChatMessageRepository chatMessageRepository;
 
     private final String ROOMS_QUERY = "SELECT r.id, r.roomname FROM infolab.rooms r ";
+    private final String ROOMS_QUERY_JOINED = ROOMS_QUERY + "RIGHT JOIN infolab.room_subscriptions s " +
+                                                            "ON r.id = s.room_id " +
+                                                            "RIGHT JOIN infolab.users u " +
+                                                            "%s " +
+                                                            "ON r.id = s.user_id ";
+    private final String NEW_ACCESS_CONDITION = "WHERE s.user_id = ? %s";
     private final String ROOMS_DISTINCT_ON_QUERY = "SELECT DISTINCT ON (r.roomname) r.roomname, " +
                                                     "m.id, m.sender_id, m.recipient_room_id, m.sent_at, m.\"content\" " +
                                                     "FROM infolab.chatmessages m " +
                                                     "LEFT JOIN infolab.rooms r " +
                                                     "ON r.id = m.recipient_room_id %s " + // Questo è per aggiungere condizioni all'on
-                                                    "%s " + // Questo è per aggiungere WHERE
+                                                    "%s " + // Questo è per aggiungere WHERE o altre JOIN
                                                     "order by r.roomname, sent_at desc ";
     private final String ACCESS_CONDITION_QUERY =
             "(r.roomname LIKE ? || '-%' OR r.roomname LIKE '%-' || ? OR r.roomname = 'general')";
@@ -63,10 +69,7 @@ public class RoomRepository {
      * @return id della room con il nome passato a parametro. -1 in caso la room non esista.
      */
     public Optional<RoomEntity> getByRoomName(String roomName, String username) {
-        if (roomName.equals("general") || roomName.contains(username + "-") || roomName.contains("-" + username))
-            return queryRoom(String.format("%s WHERE roomname = ?", ROOMS_QUERY), roomName);
-
-        return Optional.empty();
+        return queryRoom(addConditionToRoomsQueryJoined(" AND r.roomname = ? "), username, roomName);
     }
 
     // Questo metodo è necessario perché altrimenti nella creazione della RoomSubscription in ChatController
@@ -84,7 +87,7 @@ public class RoomRepository {
      * @return oggetto Room con il nome preso dal db. Ritorna null se la room non esiste.
      */
     public Optional<RoomEntity> getById(long id, String username) {
-        return queryRoom(String.format("%s WHERE id = ? AND %s", ROOMS_QUERY, ACCESS_CONDITION_QUERY), id, username, username);
+        return queryRoom(addConditionToRoomsQueryJoined("r.id = ?"), username, id);
     }
 
     private Optional<RoomEntity> queryRoom(String query, Object... queryParams) {
@@ -136,7 +139,12 @@ public class RoomRepository {
         return room;
     }
 
-    private String addConditionToRoomsDistinctOnQuery(String onCondition) {
-        return String.format(ROOMS_DISTINCT_ON_QUERY, onCondition, WHERE_ACCESS_CONDITION_QUERY);
+    private String addConditionToRoomsDistinctOnQuery(String condition) {
+        return String.format(ROOMS_DISTINCT_ON_QUERY, condition, WHERE_ACCESS_CONDITION_QUERY);
+    }
+
+    private String addConditionToRoomsQueryJoined(String condition) {
+        String partialQuery = String.format(NEW_ACCESS_CONDITION, condition);
+        return String.format(ROOMS_QUERY_JOINED, partialQuery);
     }
 }
