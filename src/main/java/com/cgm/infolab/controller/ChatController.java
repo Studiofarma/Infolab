0eb5b2bb5797195228c17fc2b83e15ad7f2cf0f4
Lorenzo Rinaldi <lorenzo.rinaldi.job@gmail.com>
2023-03-08 18:27:36 +0100

    Refactoring. Creato service.

--- src/main/java/com/cgm/infolab/controller/ChatController.java
+++ src/main/java/com/cgm/infolab/controller/ChatController.java
@@ -8,6 +8,7 @@ import com.cgm.infolab.db.repository.RoomSubscriptionRepository;
 import com.cgm.infolab.db.repository.UserRepository;
 import com.cgm.infolab.model.ChatMessageDto;
 import com.cgm.infolab.service.ChatService;
+import com.cgm.infolab.service.RoomSubscriptionService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +18,6 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.security.core.Authentication;
@@ -30,7 +30,7 @@ public class ChatController {
 
     //@Autowired
     // Tutorial: https://www.baeldung.com/spring-websockets-send-message-to-user
-    public SimpMessagingTemplate messagingTemplate;
+    //public SimpMessagingTemplate messagingTemplate;
 
     // Tutorial: https://www.baeldung.com/spring-jdbc-jdbctemplate#the-jdbctemplate-and-running-queries
     // Saltate pure la sezione 2 Configuration. Dopo avere letto come fare query di lettura e scrittura, vi
@@ -41,22 +41,18 @@ public class ChatController {
     //public JdbcTemplate jdbcTemplate;
 
     private final UserRepository userRepository;
-    private final RoomRepository roomRepository;
-    private final RoomSubscriptionRepository roomSubscriptionRepository;
     private final ChatService chatService;
+    private final RoomSubscriptionService roomSubscriptionService;
 
     private final Logger log = LoggerFactory.getLogger(ChatController.class);
 
     @Autowired
-    public ChatController(SimpMessagingTemplate messagingTemplate,
-                          UserRepository userRepository,
-                          RoomRepository roomRepository, RoomSubscriptionRepository roomSubscriptionRepository,
-                          ChatService chatService) {
-        this.messagingTemplate = messagingTemplate;
+    public ChatController(UserRepository userRepository,
+                          ChatService chatService,
+                          RoomSubscriptionService roomSubscriptionService) {
         this.userRepository = userRepository;
-        this.roomRepository = roomRepository;
-        this.roomSubscriptionRepository = roomSubscriptionRepository;
         this.chatService = chatService;
+        this.roomSubscriptionService = roomSubscriptionService;
     }
 
     // Questo metodo in teoria viene chiamato quando un utente entra nella chat general.
@@ -69,26 +65,7 @@ public class ChatController {
             log.info(String.format("User username=\"%s\" già esistente nel database", principal.getName()));
         }
 
-        RoomSubscriptionEntity roomSubscription = RoomSubscriptionEntity.empty();
-        try {
-            RoomEntity room = roomRepository.getByRoomNameEvenIfNotSubscribed("general").orElseGet(() -> {
-                // Qui dentro non dovrebbe mai entrarci, dato che la room general viene aggiunta al lancio dell'app
-                return null;
-            });
-
-            UserEntity user = userRepository.getByUsername(principal.getName()).orElseGet(() -> {
-                // Qui dentro non dovrebbe mai entrarci, dato che l'utente se non c'era è stato aggiunto
-                log.info(String.format("User username=\"%s\" non trovato.", principal.getName()));
-                return null;
-            });
-
-            roomSubscription.setRoomId(room.getId());
-            roomSubscription.setUserId(user.getId());
-
-            roomSubscriptionRepository.add(roomSubscription);
-        } catch (DuplicateKeyException e) {
-            log.info("RoomSubscription " + roomSubscription + "già esistente nel database");
-        }
+        roomSubscriptionService.subscribeUserToRoom("general", principal.getName());
     }
 
     @MessageMapping("/chat.register")
