6c1c415f5218391e425a9ad40cc45d80475886fb
LORENZO-PELLEGRINELLI <pellegrinellil06@gmail.com>
2023-03-14 18:49:46 +0100

    Ultimazione Componente

--- src/frontend/components/Button-text.js
+++ src/frontend/components/Button-text.js
@@ -13,8 +13,8 @@ export class ButtonText extends LitElement {
   static styles = css`
     #preview_btn {
       margin-left: auto;
-      padding: 7px 12px;
-      border-radius: 45px;
+      padding: 8px 14px;
+      border-radius: 30px;
       background: white;
       min-width: 80px;
       text-align: center;
@@ -22,19 +22,39 @@ export class ButtonText extends LitElement {
       outline: none;
       font-weight: bold;
       cursor: pointer;
+      transition: all 0.3s ease-in-out;
+    }
+
+    .button-animate {
+      animation: text-change-animation 0.5s ease-in-out;
+    }
+
+    @keyframes text-change-animation {
+      0% {
+        transform: scale(1);
+      }
+      50% {
+        transform: scale(1.2);
+      }
+      100% {
+        transform: scale(1);
+      }
     }
   `;
 
   render() {
     return html`
       <button @click="${this.change}" id="preview_btn">
-        ${this.openPreview ? "Chiudi la Preview" : "Apri preview"}
+        ${this.openPreview ? "Chiudi la Preview" : "Apri la Preview"}
       </button>
     `;
   }
 
   change() {
     this.openPreview = !this.openPreview;
+    const button = this.shadowRoot.querySelector("#preview_btn");
+    button.classList.add("button-animate");
+    setTimeout(() => button.classList.remove("button-animate"), 500);
   }
 }
 
