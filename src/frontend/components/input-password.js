c0b4735fe0cd3f5b387c19ddfa95d76999a269c5
Davide <daviderichini@gmail.com>
2023-03-14 18:19:44 +0100

    fix grafico aggiunto cambio visibilità

--- src/frontend/components/input-password.js
+++ src/frontend/components/input-password.js
@@ -3,7 +3,7 @@ import { LitElement, html, css } from "lit";
 import { InputField } from "./input-field";
 
 export class password extends InputField {
-  static properties() {
+  static get properties() {
     return {
       pswVisibility: false,
     };
@@ -14,12 +14,25 @@ export class password extends InputField {
       width: 100%;
     }
 
-    #container {
-      width: 100%;
-    }
-
     #input {
       font: inherit;
+      border: none;
+      grid-area: input;
+    }
+
+    il-button-icon {
+      opacity: 0;
+      grid-area: icon;
+    }
+
+    il-button-icon:hover {
+      opacity: 1;
+    }
+
+    div {
+      display: grid;
+      grid-template-areas: "input  icon";
+      grid-template-columns: 1fr 40px;
       position: relative;
       width: 100%;
       height: 40px;
@@ -29,33 +42,38 @@ export class password extends InputField {
       font-size: 15pt;
       transition: 0.5s;
       border-radius: 10px;
-    }
-
-    il-button-icon {
-      opacity: 0;
-    }
-
-    il-button-icon:hover {
-      opacity: 1;
+      background-color: white;
     }
   `;
 
   render() {
     return html`
       ${this.title === "" ? html`` : html`<label>${this.title}</label>`}
-      <input
-        placeholder="${this.placeholder}"
-        id="input"
-        @input=${this.setValue}
-        @blur="${this.setBlur}"
-        @focus="${this.setFocus}"
-      />
+      <div>
+        <input
+          placeholder="${this.placeholder}"
+          id="input"
+          @input=${this.setValue}
+          @blur="${this.setBlur}"
+          @focus="${this.setFocus}"
+          type=${!this.pswVisibility ? "password" : "text"}
+        />
 
-      <il-button-icon
-        @click=${this.setVisibility}
-        content="${!this.pswVisibility ? "mdiEye" : "mdiEyeOff"}"
-      ></il-button-icon>
+        <il-button-icon
+          @click=${this.setVisibility}
+          content="${!this.pswVisibility ? "mdiEye" : "mdiEyeOff"}"
+        ></il-button-icon>
+      </div>
     `;
+  }
+
+  setFocus() {
+    this.renderRoot.querySelector("div").style.border = "solid 2px #009C3E";
+    this.renderRoot.querySelector("input").style.outline = "none";
+  }
+
+  setBlur() {
+    this.renderRoot.querySelector("div").style.border = "solid 2px #5A9BFB";
   }
 
   setVisibility() {
