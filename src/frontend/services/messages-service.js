898fbced48960d5daa7ad70d2577a071d2c397b1
Paolo Venturi <paolo.venturi@cgm.com>
2023-03-30 15:51:54 +0200

    caricamento di un messaggio dal be

--- src/frontend/services/messages-service.js
+++ src/frontend/services/messages-service.js
@@ -1,7 +1,14 @@
 const axios = require("axios").default;
 
 export class MessagesService {
-  static async getMessagesById(id) {
-    return axios.get(`http://localhost:3000/messages?id=${id}`);
+  static async getMessagesById(username, password) {
+    return axios({
+      url: `/api/messages/general`,
+      method: "get",
+      auth: {
+        username: username,
+        password: password,
+      },
+    });
   }
 }
