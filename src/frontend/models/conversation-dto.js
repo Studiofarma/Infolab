ef1ed23b47a8153a48c53850ad78abbe9666eebd
Luca Minini <lucaminini05@gmail.com>
2023-06-01 14:47:53 +0200

    Fixed name room

--- src/frontend/models/conversation-dto.js
+++ src/frontend/models/conversation-dto.js
@@ -7,7 +7,7 @@ export class ConversationDto {
 
   constructor(obj) {
     this.id = obj.id !== undefined ? obj.id : 0;
-    this.name = obj.name !== undefined ? obj.name : "";
+    this.name = obj.roomName !== undefined ? obj.roomName : "";
     this.avatar = obj.avatar !== undefined ? obj.avatar : "";
     this.lastMessage = obj.lastMessage !== undefined ? obj.lastMessage : "";
     this.unread = obj.unread !== undefined ? obj.unread : 0;
