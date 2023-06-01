1136be78ab7ce6d5696c34480c53235e4c3563ff
Mattia Pedersoli <mattiapedersoli786@gmail.com>
2023-06-01 10:48:01 +0200

    style fix

--- src/frontend/pages/chat/header/chat-header.js
+++ src/frontend/pages/chat/header/chat-header.js
@@ -4,52 +4,52 @@ import "../../../components/button-icon";
 import { IconNames } from "../../../enums/icon-names";
 
 export class ChatHeader extends LitElement {
-  static get properties() {
-    return {
-      username: "",
-    };
-  }
+	static get properties() {
+		return {
+			username: "",
+		};
+	}
 
-  static styles = css`
-    .chatHeader {
-      background: #083c72;
-      box-shadow: 0px 1px 5px black;
-      height: 40px;
-      padding: 15px 30px;
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      color: white;
-      position: fixed;
-      width: 100%;
-      z-index: 1000;
-    }
+	static styles = css`
+		.chatHeader {
+			background: #083c72;
+			box-shadow: 0px 1px 5px black;
+			height: 40px;
+			padding: 15px 30px;
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			color: white;
+			position: fixed;
+			width: 100%;
+			z-index: 1000;
+		}
 
-    .chatHeader .settings {
-      order: 2;
-      display: flex;
-    }
+		.chatHeader .settings {
+			order: 2;
+			display: flex;
+		}
 
-    .chatHeader .contact {
-      order: 1;
-      display: flex;
-      gap: 1em;
-    }
-  `;
+		.chatHeader .contact {
+			order: 1;
+			display: flex;
+			gap: 1em;
+		}
+	`;
 
-  render() {
-    return html`
-      <div class="chatHeader">
-        <div class="settings">
-          <il-button-icon content=${IconNames.option}></il-button-icon>
-        </div>
+	render() {
+		return html`
+			<div class="chatHeader">
+				<div class="settings">
+					<il-button-icon content=${IconNames.option}></il-button-icon>
+				</div>
 
-        <div class="contact">
-          <h2>ChatBox ${this.username}</h2>
-        </div>
-      </div>
-    `;
-  }
+				<div class="contact">
+					<h2>ChatBox ${this.username}</h2>
+				</div>
+			</div>
+		`;
+	}
 }
 
 customElements.define("il-chat-header", ChatHeader);
