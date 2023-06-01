1d6336a409b3a24adf6cfb31479f5051b0b84f03
Luca Minini <lucaminini05@gmail.com>
2023-06-01 08:44:34 +0200

    Fixed bugs

--- src/frontend/pages/chat/input/editor/editor.js
+++ src/frontend/pages/chat/input/editor/editor.js
@@ -6,361 +6,346 @@ import "../../../../components/button-text";
 
 import { IconNames } from "../../../../enums/icon-names";
 import { MarkdownService } from "../../../../services/markdown-services";
+import { mdiFormatListText } from "@mdi/js";
 
 export class Editor extends LitElement {
-  static properties = {
-    message: "",
-    openPreview: false,
-    selectedText: { startingPoint: NaN, endingPoint: NaN },
-  };
+	static properties = {
+		message: "",
+		openPreview: mdiFormatListText,
+	};
 
-  constructor() {
-    super();
-    this.message = "";
-    this.openPreview = false;
-    this.selectedText = null;
-  }
+	constructor() {
+		super();
+		this.message = "";
+		this.openPreview = false;
+	}
 
-  static styles = css`
-    * {
-      box-sizing: border-box;
-      padding: 0;
-      margin: 0;
-    }
+	static styles = css`
+		* {
+			box-sizing: border-box;
+			padding: 0;
+			margin: 0;
+		}
 
-    ul,
-    ol {
-      list-style-position: inside;
-    }
+		ul,
+		ol {
+			list-style-position: inside;
+		}
 
-    .formatting-bar {
-      width: 100%;
-      background: #bcc7d9;
-      display: flex;
-      align-items: center;
-      gap: 10px;
-      padding: 0px 10px;
-    }
+		.formatting-bar {
+			width: 100%;
+			background: #bcc7d9;
+			display: flex;
+			align-items: center;
+			gap: 10px;
+			padding: 0px 10px;
+		}
 
-    textarea {
-      width: 100%;
-      height: 150px;
-      border: none;
-      outline: none;
-      padding: 5px;
-      resize: none;
-      overflow: auto;
-      font-family: inherit;
-    }
+		il-button-text {
+			margin-left: auto;
+			/* max-width: 200px;
+			width: 100%; */
+		}
 
-    div[class*="select-"] {
-      position: relative;
-      width: max-content;
-    }
+		textarea {
+			width: 100%;
+			height: 150px;
+			border: none;
+			outline: none;
+			padding: 5px;
+			resize: none;
+			overflow: auto;
+			font-family: inherit;
+		}
 
-    div[class*="select-"] .dropdown {
-      overflow: hidden;
-      max-height: 0px;
-      transition: 0.5s;
-      position: absolute;
-      top: 30px;
-      left: 0px;
-      min-width: 100px;
-      background: #bcc7d9;
-      z-index: 10;
-    }
+		div[class*="select-"] {
+			position: relative;
+			width: max-content;
+		}
 
-    div[class*="select-"]:hover .dropdown {
-      overflow-y: auto;
-      max-height: 100px;
-      padding: 10px 8px;
-    }
+		div[class*="select-"] .dropdown {
+			overflow: hidden;
+			max-height: 0px;
+			transition: 0.5s;
+			position: absolute;
+			top: 30px;
+			left: 0px;
+			min-width: 100px;
+			background: #bcc7d9;
+			z-index: 10;
+		}
 
-    .dropdown::-webkit-scrollbar {
-      width: 3px;
-      background: #677fa5;
-    }
+		div[class*="select-"]:hover .dropdown {
+			overflow-y: auto;
+			max-height: 100px;
+			padding: 10px 8px;
+		}
 
-    .dropdown::-webkit-scrollbar-thumb {
-      background: #507cc4;
-    }
+		.dropdown::-webkit-scrollbar {
+			width: 3px;
+			background: #677fa5;
+		}
 
-    .dropdown .option {
-      cursor: pointer;
-      transition: 0.5s;
-      padding: 2px;
-      display: flex;
-      align-items: center;
-      white-space: nowrap;
-    }
+		.dropdown::-webkit-scrollbar-thumb {
+			background: #507cc4;
+		}
 
-    .dropdown .option:hover {
-      background: #9faec5;
-    }
+		.dropdown .option {
+			cursor: pointer;
+			transition: 0.5s;
+			padding: 2px;
+			display: flex;
+			align-items: center;
+			white-space: nowrap;
+		}
 
-    .dropdown input[type="radio"] {
-      display: block;
-      margin-left: 25px;
-    }
+		.dropdown .option:hover {
+			background: #9faec5;
+		}
 
-    .previewer {
-      padding: 10px;
-      width: 100%;
-      height: calc(100% - 40px);
-      overflow-y: auto;
-    }
-  `;
+		.dropdown input[type="radio"] {
+			display: block;
+			margin-left: 25px;
+		}
 
-  render() {
-    return html`
-      <!-- diventerà un componente -->
-      <div class="formatting-bar">
-        <il-formatting-button
-          content=${IconNames.bold}
-          @click=${this.insertBold}
-        ></il-formatting-button>
-        <il-formatting-button
-          content=${IconNames.italic}
-          @click=${this.insertItalic}
-        ></il-formatting-button>
-        <il-formatting-button
-          content=${IconNames.strikethrough}
-          @click=${this.insertStrike}
-        ></il-formatting-button>
-        <il-formatting-button
-          content=${IconNames.link}
-          @click=${this.insertLink}
-        ></il-formatting-button>
-        <il-formatting-button
-          content=${IconNames.minus}
-          @click=${this.insertLine}
-        ></il-formatting-button>
+		.previewer {
+			padding: 5px;
+			width: 100%;
+			height: calc(100% - 40px);
+			background: #ffffff;
+			overflow-y: auto;
+			cursor: default;
+		}
+	`;
 
-        <div class="select-list">
-          <il-formatting-button
-            content=${IconNames.listBulleted}
-            @click=${this.insertList}
-          ></il-formatting-button>
-          <div class="dropdown">
-            <div class="option">
-              <label for="disc">
-                <ul>
-                  <li>example</li>
-                  <li>example</li>
-                  <li>example</li>
-                </ul>
-              </label>
-              <input type="radio" name="forList" id="disc" checked />
-            </div>
+	render() {
+		return html`
+			<!-- diventerà un componente -->
+			<div class="formatting-bar">
+				<il-formatting-button
+					content=${IconNames.bold}
+					@click=${this.insertBold}
+				></il-formatting-button>
+				<il-formatting-button
+					content=${IconNames.italic}
+					@click=${this.insertItalic}
+				></il-formatting-button>
+				<il-formatting-button
+					content=${IconNames.strikethrough}
+					@click=${this.insertStrike}
+				></il-formatting-button>
+				<il-formatting-button
+					content=${IconNames.link}
+					@click=${this.insertLink}
+				></il-formatting-button>
+				<il-formatting-button
+					content=${IconNames.minus}
+					@click=${this.insertLine}
+				></il-formatting-button>
 
-            <div class="option">
-              <label for="number">
-                <ol>
-                  <li>example</li>
-                  <li>example</li>
-                  <li>example</li>
-                </ol>
-              </label>
-              <input type="radio" name="forList" id="number" />
-            </div>
-          </div>
-        </div>
+				<div class="select-list">
+					<il-formatting-button
+						content=${IconNames.listBulleted}
+						@click=${this.insertList}
+					></il-formatting-button>
+					<div class="dropdown">
+						<div class="option">
+							<label for="disc">
+								<ul>
+									<li>example</li>
+									<li>example</li>
+									<li>example</li>
+								</ul>
+							</label>
+							<input type="radio" name="forList" id="disc" checked />
+						</div>
 
-        <div class="select-heading">
-          <il-formatting-button
-            content=${IconNames.title}
-            @click=${this.insertHeading}
-          ></il-formatting-button>
-          <div class="dropdown">
-            <div class="option">
-              <label for="h1">
-                <h1>Titolo 1</h1>
-              </label>
-              <input type="radio" name="forHeading" id="h1" checked />
-            </div>
-            <div class="option">
-              <label for="h2">
-                <h1>Titolo 2</h1>
-              </label>
-              <input type="radio" name="forHeading" id="h2" />
-            </div>
-            <div class="option">
-              <label for="h3">
-                <h1>Titolo 3</h1>
-              </label>
-              <input type="radio" name="forHeading" id="h3" />
-            </div>
-          </div>
-        </div>
+						<div class="option">
+							<label for="number">
+								<ol>
+									<li>example</li>
+									<li>example</li>
+									<li>example</li>
+								</ol>
+							</label>
+							<input type="radio" name="forList" id="number" />
+						</div>
+					</div>
+				</div>
 
-        <il-button-text
-          @click=${this.togglePreviewer}
-          text=${this.openPreview ? "Chiudi preview" : "Apri preview "}
-        ></il-button-text>
-      </div>
+				<div class="select-heading">
+					<il-formatting-button
+						content=${IconNames.title}
+						@click=${this.insertHeading}
+					></il-formatting-button>
+					<div class="dropdown">
+						<div class="option">
+							<label for="h1">
+								<h1>Titolo 1</h1>
+							</label>
+							<input type="radio" name="forHeading" id="h1" checked />
+						</div>
+						<div class="option">
+							<label for="h2">
+								<h1>Titolo 2</h1>
+							</label>
+							<input type="radio" name="forHeading" id="h2" />
+						</div>
+						<div class="option">
+							<label for="h3">
+								<h1>Titolo 3</h1>
+							</label>
+							<input type="radio" name="forHeading" id="h3" />
+						</div>
+					</div>
+				</div>
 
-      ${!this.openPreview
-        ? html`<textarea
-            placeholder="Scrivi un messaggio..."
-            @input=${this.onMessageInput}
-            @mouseup=${this.setSelectedText}
-            @keydown=${this.checkList}
-            .value=${this.message}
-          >
-          </textarea>`
-        : html`<div class="previewer">
-            ${resolveMarkdown(MarkdownService.parseMarkdown(this.message))}
-          </div>`}
-    `;
-  }
+				<il-button-text
+					@click=${this.togglePreviewer}
+					text=${this.openPreview ? "Chiudi preview" : "Apri preview "}
+				></il-button-text>
+			</div>
 
-  onMessageInput(e) {
-    const inputEl = e.target;
-    this.message = inputEl.value;
-  }
+			${!this.openPreview
+				? html`<textarea
+						placeholder="Scrivi un messaggio..."
+						@input=${this.onMessageInput}
+						@keydown=${this.checkList}
+						.value=${this.message}
+				  >
+				  </textarea>`
+				: html`<div class="previewer">
+						${resolveMarkdown(MarkdownService.parseMarkdown(this.message))}
+				  </div>`}
+		`;
+	}
 
-  togglePreviewer() {
-    this.openPreview = !this.openPreview;
-  }
+	onMessageInput(e) {
+		const inputEl = e.target;
+		this.message = inputEl.value;
+	}
 
-  setSelectedText() {
-    const textarea = this.getTextarea();
+	togglePreviewer() {
+		this.openPreview = !this.openPreview;
+	}
 
-    this.selectedText = {
-      startingPoint: textarea.selectionStart,
-      endingPoint: textarea.selectionEnd,
-    };
+	checkList(event) {
+		if (event.key === "Enter") {
+			const rows = this.message.split("\n");
+			let last_row = rows[rows.length - 1];
+			const indexOfPoint = last_row.indexOf(".");
 
-    this.dispatchEvent(
-      new CustomEvent("is-selecting", {
-        detail: {
-          start: this.selectedText.startingPoint,
-          end: this.selectedText.endingPoint,
-        },
-      })
-    );
-  }
+			if (last_row.startsWith("* ")) {
+				this.message += "\n* ";
+				event.preventDefault();
+				return;
+			}
 
-  checkList(event) {
-    if (event.key === "Enter") {
-      const rows = this.message.split("\n");
-      let last_row = rows[rows.length - 1];
-      const indexOfPoint = last_row.indexOf(".");
+			if (
+				indexOfPoint != -1 &&
+				!isNaN(parseInt(last_row.slice(0, indexOfPoint))) &&
+				last_row.startsWith(". ", indexOfPoint)
+			) {
+				this.message +=
+					"\n" +
+					(parseInt(last_row.slice(0, indexOfPoint)) + 1).toString() +
+					". ";
+				event.preventDefault();
+				return;
+			}
+		}
+	}
 
-      if (last_row.startsWith("* ")) {
-        this.message += "\n* ";
-        event.preventDefault();
-        return;
-      }
+	insertInTextArea(str) {
+		let textarea = this.getTextarea();
+		let start = textarea.selectionStart;
+		let finish = textarea.selectionEnd;
 
-      if (
-        indexOfPoint != -1 &&
-        !isNaN(parseInt(last_row.slice(0, indexOfPoint))) &&
-        last_row.startsWith(". ", indexOfPoint)
-      ) {
-        this.message +=
-          "\n" +
-          (parseInt(last_row.slice(0, indexOfPoint)) + 1).toString() +
-          ". ";
-        event.preventDefault();
-        return;
-      }
-    }
-  }
+		this.message =
+			this.message.slice(0, start) + str + this.message.slice(finish);
+	}
 
-  insertInTextArea(str) {
-    if (this.selectedText !== null) {
-      this.message =
-        this.message.slice(0, this.selectedText.startingPoint) +
-        str +
-        this.message.slice(this.selectedText.endingPoint);
-    } else {
-      this.message = this.message + str;
-    }
-    this.selectedText = null;
-  }
+	//funzioni per formatting-buttons
 
-  //funzioni per formatting-buttons
+	getText(text) {
+		const sel = window.getSelection();
+		const t = sel ? sel.toString() : "";
+		if (t !== "") sel.deleteFromDocument();
+		else t = text;
+		return t;
+	}
 
-  getText(text) {
-    const sel = window.getSelection();
-    const t = sel ? sel.toString() : "";
-    if (t !== "") sel.deleteFromDocument();
-    else t = text;
-    return t;
-  }
+	insertBold() {
+		const text = this.getText("grassetto");
+		this.insertInTextArea("**" + text + "**");
+	}
 
-  insertBold() {
-    const text = this.getText("grassetto");
-    this.insertInTextArea("**" + text + "**");
-  }
+	insertItalic() {
+		const text = this.getText("italic");
+		this.insertInTextArea("*" + text + "*");
+	}
 
-  insertItalic() {
-    const text = this.getText("italic");
-    this.insertInTextArea("*" + text + "*");
-  }
+	insertStrike() {
+		const text = this.getText("barrato");
+		this.insertInTextArea("~~" + text + "~~");
+	}
 
-  insertStrike() {
-    const text = this.getText("barrato");
-    this.insertInTextArea("~~" + text + "~~");
-  }
+	insertLink() {
+		const text = this.getText("testo");
+		this.insertInTextArea("[" + text + "](link)");
+	}
 
-  insertLink() {
-    const text = this.getText("testo");
-    this.insertInTextArea("[" + text + "](link)");
-  }
+	insertLine() {
+		this.insertInTextArea("\n - - - \n");
+	}
 
-  insertLine() {
-    this.insertInTextArea("\n - - - \n");
-  }
+	insertList() {
+		const text = this.getText("punto");
+		this.insertInTextArea(this.getTypeOfList(text));
+	}
 
-  insertList() {
-    const text = this.getText("punto");
-    this.insertInTextArea(this.getTypeOfList(text));
-  }
+	insertHeading() {
+		const text = this.getText("Titolo");
+		this.insertInTextArea(this.getTypeOfHeading(text));
+	}
 
-  insertHeading() {
-    const text = this.getText("Titolo");
-    this.insertInTextArea(this.getTypeOfHeading(text));
-  }
+	getTypeOfList(text) {
+		const checkedList =
+			this.renderRoot.querySelector(`input[name="forList"]:checked`) ?? null;
 
-  getTypeOfList(text) {
-    const checkedList =
-      this.renderRoot.querySelector(`input[name="forList"]:checked`) ?? null;
+		switch (checkedList.id) {
+			case "disc":
+				return "* " + text;
+			case "number":
+				return "1. " + text;
+		}
+	}
 
-    switch (checkedList.id) {
-      case "disc":
-        return "* " + text;
-      case "number":
-        return "1. " + text;
-    }
-  }
+	getTypeOfHeading(text) {
+		const checkedHeading =
+			this.renderRoot.querySelector(`input[name="forHeading"]:checked`) ?? null;
 
-  getTypeOfHeading(text) {
-    const checkedHeading =
-      this.renderRoot.querySelector(`input[name="forHeading"]:checked`) ?? null;
+		switch (checkedHeading.id) {
+			case "h1":
+				return "# " + text;
+			case "h2":
+				return "## " + text;
+			case "h3":
+				return "### " + text;
+		}
+	}
 
-    switch (checkedHeading.id) {
-      case "h1":
-        return "# " + text;
-      case "h2":
-        return "## " + text;
-      case "h3":
-        return "### " + text;
-    }
-  }
+	getTextarea() {
+		return this.renderRoot.querySelector("textarea") ?? null;
+	}
 
-  getTextarea() {
-    return this.renderRoot.querySelector("textarea") ?? null;
-  }
-
-  updated(changed) {
-    if (changed.has("message")) {
-      this.dispatchEvent(
-        new CustomEvent("typing-text", { detail: { content: this.message } })
-      );
-      this.getTextarea().focus();
-    }
-  }
+	updated(changed) {
+		if (changed.has("message")) {
+			this.dispatchEvent(
+				new CustomEvent("typing-text", { detail: { content: this.message } })
+			);
+			this.getTextarea().focus();
+		}
+	}
 }
 customElements.define("il-editor", Editor);
