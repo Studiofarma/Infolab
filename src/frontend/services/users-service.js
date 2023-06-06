860dade4a52e44e5b28376bc581763f5c4eed8c6
Mattia Pedersoli <mattiapedersoli786@gmail.com>
2023-06-06 13:41:21 +0200

    Connected FE to BE

--- src/frontend/services/users-service.js
+++ src/frontend/services/users-service.js
@@ -1,14 +1,13 @@
 const axios = require("axios").default;
 
-
 export class UsersService {
-    static async GetUsers() {
-        return axios({
-            url: "http://localhost:3000/users",
-            method: "get",
-            headers: {
-                "X-Requested-With": "XMLHttpRequest",
-            },
-        });
-    }
+	static async GetUsers(query) {
+		return axios({
+			url: `/api/users?user=${query}`,
+			method: "get",
+			headers: {
+				"X-Requested-With": "XMLHttpRequest",
+			},
+		});
+	}
 }
