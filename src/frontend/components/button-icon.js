96b5acd964cd04dbaf2864c34eebb57ade934cb5
Mattia Pedersoli <mattiapedersoli786@gmail.com>
2023-06-09 15:33:22 +0200

    Restyle editor component

--- src/frontend/components/button-icon.js
+++ src/frontend/components/button-icon.js
@@ -1,13 +1,14 @@
 import { LitElement, html, css } from "lit";
 
 import "./icon";
+import { ConversationDto } from "./../models/conversation-dto";
 
 export class ButtonIcon extends LitElement {
-  static get properties() {
-    return {
-      content: "",
-    };
-  }
+	static get properties() {
+		return {
+			content: "",
+		};
+	}
 
   static styles = css`
     :host {
@@ -15,33 +16,30 @@ export class ButtonIcon extends LitElement {
       align-items: center;
     }
 
-    .icon-button {
-      height: 100%;
-      width: 100%;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      font-size: 0px;
-      -webkit-font-smoothing: antialiased;
-      cursor: pointer;
-      user-select: none;
-      min-height: 35px;
-      min-width: 35px;
-    }
+		.icon-button {
+			height: 100%;
+			width: 100%;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			font-size: 0px;
+			-webkit-font-smoothing: antialiased;
+			cursor: pointer;
+			user-select: none;
+		}
 
-    .icon-button:hover {
-      background-color: rgba(152, 154, 157, 0.3);
-      border-radius: 50%;
-    }
-  `;
+		.icon-button:hover {
+			color: white;
+		}
+	`;
 
-  render() {
-    return html`
-      <div class="icon-button">
-        <il-icon name="${this.content}"></il-icon>
-      </div>
-    `;
-  }
+	render() {
+		return html`
+			<div class="icon-button">
+				<il-icon name="${this.content}"></il-icon>
+			</div>
+		`;
+	}
 }
 
 customElements.define("il-button-icon", ButtonIcon);
