c8a2382f9fc0e8aaa8689990439daa130441b8a6
Lorenzo Rinaldi <lorenzo.rinaldi@cgm.com>
2023-06-16 10:37:53 +0200

    Fixed bug

--- src/main/java/com/cgm/infolab/db/repository/UserRepository.java
+++ src/main/java/com/cgm/infolab/db/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.cgm.infolab.db.repository;
 
 import com.cgm.infolab.db.model.UserEntity;
 import com.cgm.infolab.db.model.Username;
+import com.cgm.infolab.db.repository.queryhelper.QueryHelper;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
@@ -17,11 +18,17 @@ import java.util.Optional;
 @Component
 public class UserRepository {
     private final JdbcTemplate jdbcTemplate;
+    private final QueryHelper queryHelper;
     private final DataSource dataSource;
-    private final String USERS_QUERY = "SELECT * FROM infolab.users";
+    private final String USERS_SELECT = "SELECT *";
+    private final String USERS_FROM = "infolab.users";
+    private final String USERS_WHERE_ID = "id = :userId";
+    private final String USERS_WHERE_USERNAME = "username = :username";
 
-    public UserRepository(JdbcTemplate jdbcTemplate, DataSource dataSource) {
+
+    public UserRepository(JdbcTemplate jdbcTemplate, QueryHelper queryHelper, DataSource dataSource) {
         this.jdbcTemplate = jdbcTemplate;
+        this.queryHelper = queryHelper;
         this.dataSource = dataSource;
     }
 
@@ -47,7 +54,10 @@ public class UserRepository {
      * @return id dell'utente con il nome passato a parametro. -1 in caso l'utente non esista.
      */
     public Optional<UserEntity> getByUsername(Username username) {
-        return queryUser(String.format("%s WHERE username = ?", USERS_QUERY), username.value());
+        Map<String, Object> map = new HashMap<>();
+        map.put("username", username.value());
+
+        return queryUser2(USERS_SELECT, USERS_FROM, USERS_WHERE_USERNAME, map);
     }
 
     /**
@@ -56,13 +66,30 @@ public class UserRepository {
      * @return oggetto User con il nome preso dal db. Ritorna null se l'user non esiste.
      */
     public Optional<UserEntity> getById(long id) {
-        return queryUser(String.format("%s WHERE id = ?", USERS_QUERY), id);
+        Map<String, Object> map = new HashMap<>();
+        map.put("userId", id);
+
+        return queryUser2(USERS_SELECT, USERS_FROM, USERS_WHERE_ID, map);
     }
 
     private Optional<UserEntity> queryUser(String query, Object... objects) {
         try {
             return Optional.ofNullable(
                     jdbcTemplate.queryForObject(query, this::mapToEntity, objects)
+            );
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+    }
+
+    private Optional<UserEntity> queryUser2(String select, String from, String where, Map<String, ?> queryParams) {
+        try {
+            return Optional.ofNullable(
+                    queryHelper
+                            .query(select)
+                            .from(from)
+                            .where(where)
+                            .executeForObject(this::mapToEntity, queryParams)
             );
         } catch (EmptyResultDataAccessException e) {
             return Optional.empty();
