c07b739d0bfb1f2bc1525c06a177877de1cdc555
DavideRichini <daviderichini@gmail.com>
2023-06-20 10:57:37 +0200

    spostato profilo e rimossa rotellina

--- src/frontend/pages/chat/header/chat-header.js
+++ src/frontend/pages/chat/header/chat-header.js
@@ -1,6 +1,7 @@
 import { LitElement, html, css } from "lit";
 
 import "../../../components/button-icon";
+import "../../../components/avatar";
 import { IconNames } from "../../../enums/icon-names";
 
 export class ChatHeader extends LitElement {
@@ -26,43 +27,37 @@ export class ChatHeader extends LitElement {
       z-index: 1000;
     }
 
-    .chatHeader .settings {
-      order: 2;
-      display: flex;
-    }
-
     .chatHeader .contact {
       order: 1;
       display: flex;
       gap: 1em;
     }
 
-    .settings {
-      transition: 1s;
-    }
-
-    .settings:hover {
-      transform: rotate(180deg);
-    }
-
     .contact {
-      width: 95%;
+      width: 100%;
       display: flex;
       justify-content: space-between;
+    }
+
+    #profileContainer {
+      display: flex;
+    }
+
+    #profileContainer il-avatar {
+      vertical-align: center;
+      padding: 15px;
     }
   `;
 
   render() {
     return html`
       <div class="chatHeader">
-        <div class="settings">
-          <il-button-icon content=${IconNames.settings}></il-button-icon>
-        </div>
-
         <div class="contact">
           <h2>${this.roomName}</h2>
-          <!-- Username will not be here, need to be removed, only for debug -->
-          <h2>Nome utente: ${this.userName}</h2>
+          <div id="profileContainer">
+            <il-avatar name=${this.userName}></il-avatar>
+            <h2>${this.userName}</h2>
+          </div>
         </div>
       </div>
     `;
--- src/frontend/pages/chat/sidebar/sidebar.js
+++ src/frontend/pages/chat/sidebar/sidebar.js
@@ -18,7 +18,6 @@ export class Sidebar extends LitElement {
     .side-bar {
       background: #083c72;
       color: white;
-
       display: flex;
       flex-direction: column;
       height: 100vh;
@@ -28,54 +27,6 @@ export class Sidebar extends LitElement {
     }
 
     .search,
-    .profile-box {
-      flex: 0 0 auto;
-    }
-
-    .profile-box {
-      position: relative;
-      display: flex;
-      align-items: center;
-      gap: 20px;
-      cursor: pointer;
-      transition: transform 0.5s;
-    }
-
-    .profile-box:hover {
-      transform: translateY(-95px);
-    }
-
-    .profile-avatar {
-      transition: transform 0.5s;
-    }
-
-    .profile-box:hover .profile-avatar {
-      transform: scale(1.5);
-    }
-
-    .profile-tab-name {
-      transition: opacity 0.5s;
-    }
-
-    .profile-box:hover .profile-tab-name {
-      display: none;
-    }
-
-    .profile-username {
-      opacity: 0;
-    }
-
-    .profile-box:hover .profile-username {
-      opacity: 1;
-    }
-
-    .profile-info {
-      position: absolute;
-      background: #083c72;
-      width: 100%;
-      top: 70px;
-    }
-
     .conversation-list {
       flex: 1 0 auto;
       margin-right: 5px;
@@ -97,20 +48,6 @@ export class Sidebar extends LitElement {
           @search-chat="${this.searchChat}"
         ></il-search>
         <il-conversation-list class="conversation-list"></il-conversation-list>
-        <div class="profile-box">
-          <il-avatar
-            class="profile-avatar"
-            style="padding: 10px"
-            .avatarLink=${""}
-            .name=${this.login.username}
-            .id=${0}
-          ></il-avatar>
-          <p class="profile-tab-name">Profilo</p>
-          <p class="profile-username">${this.login.username}</p>
-          <div class="profile-info">
-            <h1>${this.login.username}</h1>
-          </div>
-        </div>
       </div>
     `;
   }
