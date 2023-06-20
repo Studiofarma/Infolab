5b7bd5b9c5ef5cb37163100b26d79397baf5ec06
Davide <119447719+DavideRichini@users.noreply.github.com>
2023-06-20 12:01:05 +0200

    Fe/traduzione label (#141)
    * tradotte label
    
    * fix rebase
    
    * fix pull request
    
    * tradotti label mancanti
    
    * sistemazione indentazione verticale css
    
    * traduzione label editor

--- src/frontend/pages/chat/header/chat-header.js
+++ src/frontend/pages/chat/header/chat-header.js
@@ -4,69 +4,69 @@ import "../../../components/button-icon";
 import { IconNames } from "../../../enums/icon-names";
 
 export class ChatHeader extends LitElement {
-	static get properties() {
-		return {
-			userName: "",
-			roomName: "",
-		};
-	}
+  static get properties() {
+    return {
+      userName: "",
+      roomName: "",
+    };
+  }
 
-	static styles = css`
-		.chatHeader {
-			background: #083c72;
-			height: 40px;
-			padding: 15px 30px;
-			display: flex;
-			justify-content: space-between;
-			align-items: center;
-			color: white;
-			position: fixed;
-			width: calc(100vw - 400px);
-			border-bottom: 1px solid black;
-			z-index: 1000;
-		}
+  static styles = css`
+    .chatHeader {
+      background: #083c72;
+      height: 40px;
+      padding: 15px 30px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      color: white;
+      position: fixed;
+      width: calc(100vw - 400px);
+      border-bottom: 1px solid black;
+      z-index: 1000;
+    }
 
-		.chatHeader .settings {
-			order: 2;
-			display: flex;
-		}
+    .chatHeader .settings {
+      order: 2;
+      display: flex;
+    }
 
-		.chatHeader .contact {
-			order: 1;
-			display: flex;
-			gap: 1em;
-		}
+    .chatHeader .contact {
+      order: 1;
+      display: flex;
+      gap: 1em;
+    }
 
-		.settings {
-			transition: 1s;
-		}
+    .settings {
+      transition: 1s;
+    }
 
-		.settings:hover {
-			transform: rotate(180deg);
-		}
+    .settings:hover {
+      transform: rotate(180deg);
+    }
 
-		.contact {
-			width: 95%;
-			display: flex;
-			justify-content: space-between;
-		}
-	`;
+    .contact {
+      width: 95%;
+      display: flex;
+      justify-content: space-between;
+    }
+  `;
 
-	render() {
-		return html`
-			<div class="chatHeader">
-				<div class="settings">
-					<il-button-icon content=${IconNames.settings}></il-button-icon>
-				</div>
+  render() {
+    return html`
+      <div class="chatHeader">
+        <div class="settings">
+          <il-button-icon content=${IconNames.settings}></il-button-icon>
+        </div>
 
-				<div class="contact">
-					<h2>${this.roomName}</h2>
-					<!-- Username will not be here, need to be removed, only for debug -->
-					<h2>UserName: ${this.userName}</h2>
-				</div>
-			</div>
-		`;
-	}
+        <div class="contact">
+          <h2>${this.roomName}</h2>
+          <!-- Username will not be here, need to be removed, only for debug -->
+          <h2>Nome utente: ${this.userName}</h2>
+        </div>
+      </div>
+    `;
+  }
 }
 
 customElements.define("il-chat-header", ChatHeader);
