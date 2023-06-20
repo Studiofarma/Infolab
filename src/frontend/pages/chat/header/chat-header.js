5d0d89f2f5aabfbc22581d6823734aef4b58ebd8
DavideRichini <daviderichini@gmail.com>
2023-06-20 12:13:35 +0200

    eliminato codoce inutile

--- src/frontend/pages/chat/header/chat-header.js
+++ src/frontend/pages/chat/header/chat-header.js
@@ -1,7 +1,6 @@
 import { LitElement, html, css } from "lit";
 
 import "../../../components/button-icon";
-import "../../../components/avatar";
 import { IconNames } from "../../../enums/icon-names";
 
 export class ChatHeader extends LitElement {
