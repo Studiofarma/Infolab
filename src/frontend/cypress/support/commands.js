06c29924c9ffe4ed9ffaae07f6eb572d32735c42
Paolo Venturi <paolo.venturi@cgm.com>
2023-06-23 10:52:26 +0200

    Setup Cypress

---
+++ src/frontend/cypress/support/commands.js
@@ -0,0 +1,55 @@
+Cypress.Commands.add("getLitElement", (elementPath) => {
+  let elementNames = elementPath.includes(",")
+    ? elementPath.split(",")
+    : Array.of(elementPath);
+
+  let ref = cy;
+
+  for (let index = 0; index < elementNames.length; index++) {
+    ref =
+      index === 0
+        ? ref.get(elementNames[index]).shadow()
+        : ref.find(elementNames[index]).shadow();
+  }
+
+  return ref;
+});
+
+Cypress.Commands.add("litElementExist", (elementPath) => {
+  let elementNames = elementPath.includes(",")
+    ? elementPath.split(",")
+    : Array.of(elementPath);
+
+  let elementName = elementNames.pop();
+
+  if (elementNames.length > 0) {
+    let ref = cy.getLitElement(elementNames.toString());
+
+    return ref.find(elementName).should("exist");
+  } else {
+    return cy.get(elementName).should("exist");
+  }
+});
+
+Cypress.Commands.add("login", (user) => {
+  cy.session(
+    user,
+    () => {
+      const baseUrl = "http://localhost:8081";
+      cy.visit(baseUrl);
+
+      cy.getLitElement("il-app,il-login,il-input-field#username")
+        .find("input")
+        .type(user.user, { force: true });
+
+      cy.getLitElement("il-app,il-login,il-input-password#password")
+        .find("input")
+        .type(user.password, { force: true });
+
+      cy.getLitElement("il-app,il-login,il-button-text").find("button").click();
+    },
+    {
+      validate: () => {},
+    }
+  );
+});
