48ab9ddc53bf78fde3e7a3bc0307376f0642d36d
Lorenzo Rinaldi <lorenzo.rinaldi@cgm.com>
2023-06-27 12:01:13 +0200

    Restyle chat filter

--- src/frontend/components/input-ricerca.js
+++ src/frontend/components/input-ricerca.js
@@ -5,85 +5,92 @@ import { InputField } from "./input-field";
 import "./button-icon";
 
 export class InputRicerca extends InputField {
-	static properties = {
-		isFocus: { type: Boolean },
-	};
+  static properties = {
+    isFocus: { type: Boolean },
+  };
 
-	constructor() {
-		super();
-		this.isFocus = false;
-	}
+  constructor() {
+    super();
+    this.isFocus = false;
+  }
 
-	static styles = [
-		css`
-			div {
-				width: 100%;
-				display: flex;
-				background-color: rgb(0, 38, 78);
-				border-radius: 10px;
-				color: white;
-			}
+  static styles = [
+    css`
+      div {
+        width: 100%;
+        display: flex;
+        color: white;
+        border-radius: 10px;
+        border: solid 2px;
+      }
 
-			input {
-				width: 100%;
-				height: 40px;
-				padding: 0 10px;
-				border: none;
-				outline: none;
-				color: white;
-				background-color: rgba(0, 0, 0, 0);
-				position: relative;
-				overflow: hidden;
-			}
+      div:focus {
+        border: #5a9bfb;
+      }
 
-			il-button-icon {
-				padding: 5px;
-			}
-		`,
-	];
+      input {
+        width: 100%;
+        height: 40px;
+        padding: 0 10px;
+        border: none;
+        outline: none;
+        background-color: rgba(0, 0, 0, 0);
+        position: relative;
+        overflow: hidden;
+      }
 
-	render() {
-		return html`
-			<div>
-				<input
-					placeholder="${this.placeholder}"
-					@input="${(e) => {
-						this.search();
-						this.setValue(e);
-					}}"
-					@focus="${this.toggleFocus}"
-					@blur=${this.toggleFocus}
-				/>
-				<il-button-icon
-					@click=${() => {
-						this.clear();
-						this.search();
-					}}
-					content=${this.isFocus == true && this.value != ""
-						? IconNames.close
-						: IconNames.magnify}
-				></il-button-icon>
-			</div>
-		`;
-	}
+      il-button-icon {
+        padding: 5px;
+      }
+    `,
+  ];
 
-	search(event) {
-		let input = this.renderRoot.querySelector("div input");
-		this.dispatchEvent(
-			new CustomEvent("search", {
-				detail: {
-					query: input.value,
-				},
-				bubbles: true,
-				composed: true,
-			})
-		);
-		input.focus();
-	}
+  render() {
+    return html`
+      <div
+        .style=${this.isFocus
+          ? "border-color: #004CE3;"
+          : "border-color: #989A9D;"}
+      >
+        <input
+          placeholder="${this.placeholder}"
+          @input="${(e) => {
+            this.search();
+            this.setValue(e);
+          }}"
+          @focus="${this.toggleFocus}"
+          @blur=${this.toggleFocus}
+        />
+        <il-button-icon
+          @click=${() => {
+            this.clear();
+            this.search();
+          }}
+          content=${this.isFocus == true && this.value != ""
+            ? IconNames.close
+            : IconNames.magnify}
+        ></il-button-icon>
+      </div>
+    `;
+  }
 
-	toggleFocus() {
-		this.isFocus = !this.isFocus;
-	}
+  search() {
+    let input = this.renderRoot.querySelector("div input");
+    this.dispatchEvent(
+      new CustomEvent("search", {
+        detail: {
+          query: input.value,
+        },
+        bubbles: true,
+        composed: true,
+      })
+    );
+    input.focus();
+  }
+
+  toggleFocus() {
+    this.isFocus = !this.isFocus;
+  }
 }
 
 customElements.define("il-input-ricerca", InputRicerca);
