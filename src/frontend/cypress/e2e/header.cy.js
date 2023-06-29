3c69cfca2d7d31cf3b6db088c3bcfca409124ead
Guidetti Daniele <danieleguidetti06@gmail.com>
2023-06-29 17:30:24 +0200

    fatti i fix richiesti nella PR

--- src/frontend/cypress/e2e/header.cy.js
+++ src/frontend/cypress/e2e/header.cy.js
@@ -1,59 +1,60 @@
+const HEADER_PATH = "il-app,il-chat,il-chat-header";
+const AVATAR_PATH = "il-app,il-chat,il-chat-header, il-avatar";
+const CONVERSATION_PATH =
+  "il-app,il-chat, il-sidebar, il-conversation-list, il-conversation";
+
 beforeEach(() => {
   cy.login({ user: "user1", password: "password1" });
 });
 
 describe("header component spec", () => {
   it("header component exists after login", () => {
-    cy.litElementExist("il-app,il-chat,il-chat-header");
+    cy.litElementExist(HEADER_PATH);
   });
 
   it("profileContainers exist in the il-chat header component", () => {
-    cy.getLitElement("il-app,il-chat,il-chat-header")
-      .find(".profileContainer")
-      .should("exist");
+    cy.getLitElement(HEADER_PATH).find(".profileContainer").should("exist");
   });
 
   it("asserting that only your profileContainer exists", () => {
     // asserting user profile container exist
-    cy.getLitElement("il-app,il-chat , il-chat-header")
+    cy.getLitElement(HEADER_PATH)
       .find("il-avatar[name='Mario Rossi']")
       .should("exist");
 
     // asserting conversation profile container doesn't exist
-    cy.getLitElement("il-app,il-chat , il-chat-header")
+    cy.getLitElement(HEADER_PATH)
       .find("il-avatar:not([name='Mario Rossi'])")
       .should("not.exist");
 
     // asserting user profile container has the correct description
-    cy.getLitElement("il-app,il-chat , il-chat-header")
+    cy.getLitElement(HEADER_PATH)
       .find(".profileContainer")
       .last()
       .should("include.text", "Mario Rossi");
   });
 
   it("asserting that your avatar displays the correct initials", () => {
-    cy.getLitElement("il-app,il-chat,il-chat-header, il-avatar")
+    cy.getLitElement(AVATAR_PATH)
       .find("#avatar-default")
       .should("include.text", "MR");
   });
 
   it("get the correct avatar when you open a chat", () => {
-    cy.getLitElement(
-      "il-app,il-chat, il-sidebar, il-conversation-list, il-conversation"
-    )
+    cy.getLitElement(CONVERSATION_PATH)
       .find(".chat-name")
       .as("conversation")
       .each((el, index) => {
         cy.get("@conversation").eq(index).click({ force: true });
 
-        cy.getLitElement("il-app, il-chat, il-chat-header")
+        cy.getLitElement(HEADER_PATH)
           .find("il-avatar")
           .should("have.length", 2);
 
         cy.wrap(el)
           .invoke("text")
           .then((text) => {
-            cy.getLitElement("il-app, il-chat, il-chat-header")
+            cy.getLitElement(HEADER_PATH)
               .find(".profileContainer")
               .should("include.text", text);
           });
@@ -61,15 +62,13 @@ describe("header component spec", () => {
   });
 
   it("asserting that the avatar of a contact contains the correct initials ", () => {
-    cy.getLitElement(
-      "il-app,il-chat, il-sidebar, il-conversation-list, il-conversation"
-    )
+    cy.getLitElement(CONVERSATION_PATH)
       .find(".chat-name")
       .as("conversation")
       .each((el, index) => {
         cy.get("@conversation").eq(index).click({ force: true });
 
-        cy.getLitElement("il-app, il-chat, il-chat-header")
+        cy.getLitElement(HEADER_PATH)
           .find("il-avatar")
           .should("have.length", 2);
 
@@ -82,7 +81,7 @@ describe("header component spec", () => {
               .join("")
               .toUpperCase();
 
-            cy.getLitElement("il-app, il-chat, il-chat-header, il-avatar")
+            cy.getLitElement(AVATAR_PATH)
               .first()
               .find("#avatar-default")
               .should("include.text", initials);
--- src/frontend/cypress/support/e2e.js
+++ src/frontend/cypress/support/e2e.js
@@ -1,7 +1,1 @@
 import "./commands";
-
-Cypress.on("uncaught:exception", (err, runnable) => {
-  // returning false here prevents Cypress from
-  // failing the test
-  return false;
-});
