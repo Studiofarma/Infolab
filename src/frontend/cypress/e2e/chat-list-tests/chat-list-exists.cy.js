a83a858a02573f07d89e482140b7092b2ddefffb
DavideRichini <daviderichini@gmail.com>
2023-06-29 15:23:01 +0200

    aggiunti controlli a conversation list

--- src/frontend/cypress/e2e/chat-list-tests/chat-list-exists.cy.js
+++ src/frontend/cypress/e2e/chat-list-tests/chat-list-exists.cy.js
@@ -1,13 +1,34 @@
 describe("chat list render spec", () => {
   it("chat list exists after login", () => {
-    cy.login({ user: "user1", password: "password1" });
-
     cy.countElements(
       "il-app,il-chat,il-sidebar,il-conversation-list",
       "il-conversation",
       9
     );
   });
+
+  it("separator 'Conversazioni' exists after login", () => {
+    cy.getLitElement("il-app,il-chat,il-sidebar,il-conversation-list")
+      .find(".separator")
+      .eq(0)
+      .should("have.text", "Conversazioni");
+  });
+
+  it("separator 'Nuove conversazioni' exists after login", () => {
+    cy.getLitElement("il-app,il-chat,il-sidebar,il-conversation-list")
+      .contains("Nuove conversazioni")
+      .should("exist");
+  });
+
+  it("filter exist after login", () => {
+    cy.litElementExist(
+      "il-app,il-chat,il-sidebar,il-conversation-list,il-search"
+    );
+  });
+});
+
+beforeEach(() => {
+  cy.login({ user: "user1", password: "password1" });
 });
 
 afterEach(() => {
