f404f606926d1f5f0111b2ca43030dc400b292c0
Guidetti Daniele <danieleguidetti06@gmail.com>
2023-06-29 09:44:36 +0200

    SAVEPOINT

--- src/frontend/cypress/e2e/header.cy.js
+++ src/frontend/cypress/e2e/header.cy.js
@@ -1,55 +1,85 @@
+beforeEach(() => {
+  cy.login({ user: "user1", password: "password1" });
+});
+
 describe("header component spec", () => {
-
-
   it("header component exists after login", () => {
-    cy.login({ user: "user1", password: "password1"});
-
     cy.litElementExist("il-app,il-chat,il-chat-header");
   });
 
-
   it("profileContainers exist in the il-chat header component", () => {
-    cy.login({ user: "user1", password: "password1" });
-
     cy.getLitElement("il-app,il-chat,il-chat-header")
       .find(".profileContainer")
       .should("exist");
   });
 
-  it("check if only your profileContainer exists", () => {
-    cy.login({ user: "user1", password: "password1" });
+  it("asserting that only your profileContainer exists", () => {
 
-    cy.getLitElement("il-app,il-chat,il-chat-header")
-      .find(".profileContainer")
-      .should("have.length", 1)
-      .and("include.text", "user1");
+      cy.getLitElement("il-app,il-chat,il-chat-header")
+        .find(".profileContainer")
+        .should("have.length", 1)
+        .and("include.text", "Mario Rossi");
+
   });
 
-
-  it("check if your avatar displays the correct initials", () => {
-    cy.login({ user: "user1", password: "password1" });
-
-    cy.getLitElement("il-app,il-chat,il-chat-header, il-avatar").find("#avatar-default").should("include.text", "U")
-
+  it("asserting that your avatar displays the correct initials", () => {
+    cy.getLitElement("il-app,il-chat,il-chat-header, il-avatar")
+      .find("#avatar-default")
+      .should("include.text", "MR");
   });
 
   it("get the correct avatar when you open a chat", () => {
+    cy.getLitElement(
+      "il-app,il-chat, il-sidebar, il-conversation-list, il-conversation"
+    )
+      .find(".chat-name")
+      .as("conversation")
+      .each((el, index) => {
+        cy.get("@conversation").eq(index).click({ force: true });
 
-    cy.login({ user: "user1", password: "password1" });
+        cy.getLitElement("il-app, il-chat, il-chat-header")
+          .find("il-avatar")
+          .should("have.length", 2);
 
-    cy.getLitElement("il-app,il-chat, il-sidebar, il-conversation-list").find("il-conversation").each( ($el,index)=> {
+        cy.wrap(el)
+          .invoke("text")
+          .then((text) => {
+            cy.getLitElement("il-app, il-chat, il-chat-header")
+              .find(".profileContainer")
+              .should("include.text", text);
+          });
+      });
+  });
 
-      cy.getLitElement("il-app,il-chat, il-sidebar, il-conversation-list").find("il-conversation").eq(index).click({force: true})
+  it("asserting that the avatar of a contact contains the correct initials ", () => {
+    cy.getLitElement(
+      "il-app,il-chat, il-sidebar, il-conversation-list, il-conversation"
+    )
+      .find(".chat-name")
+      .as("conversation")
+      .each((el, index) => {
+        cy.get("@conversation").eq(index).click({ force: true });
 
-    })
+        cy.getLitElement("il-app, il-chat, il-chat-header")
+          .find("il-avatar")
+          .should("have.length", 2);
 
+        cy.wrap(el)
+          .invoke("text")
+          .then((text) => {
+            const initials = text
+              .split(" ")
+              .map((t) => t.charAt(0))
+              .join("")
+              .toUpperCase();
 
-
-
-  })
- 
-
-
+            cy.getLitElement("il-app, il-chat, il-chat-header, il-avatar")
+              .first()
+              .find("#avatar-default")
+              .should("include.text", initials);
+          });
+      });
+  });
 });
 
 afterEach(() => {
