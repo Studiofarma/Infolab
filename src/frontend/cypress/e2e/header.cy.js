dd47ea24a940493e7b377618e16d02a13d13724d
Guidetti Daniele <danieleguidetti06@gmail.com>
2023-06-29 11:13:02 +0200

    sistemata condizione di esistenza profileContainer

--- src/frontend/cypress/e2e/header.cy.js
+++ src/frontend/cypress/e2e/header.cy.js
@@ -14,12 +14,21 @@ describe("header component spec", () => {
   });
 
   it("asserting that only your profileContainer exists", () => {
+    // asserting user profile container exist
     cy.getLitElement("il-app,il-chat , il-chat-header")
-      .find("il-avatar").should("have.length",1 )
+      .find("il-avatar[name='Mario Rossi']")
+      .should("exist");
 
-    cy.getLitElement("il-app,il-chat , il-chat-header").find(".profileContainer").last().should("include.text", "Mario Rossi")
-   
+    // asserting conversation profile container doesn't exist
+    cy.getLitElement("il-app,il-chat , il-chat-header")
+      .find("il-avatar:not([name='Mario Rossi'])")
+      .should("not.exist");
 
+    // asserting user profile container has the correct description
+    cy.getLitElement("il-app,il-chat , il-chat-header")
+      .find(".profileContainer")
+      .last()
+      .should("include.text", "Mario Rossi");
   });
 
   it("asserting that your avatar displays the correct initials", () => {
