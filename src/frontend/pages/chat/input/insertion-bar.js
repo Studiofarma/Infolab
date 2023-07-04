65e13b0da9d736e3104b445b83bb8c05b9326f74
Mirko Malgarotti <malgarottimirko@gmail.com>
2023-07-04 10:59:01 +0200

    Commentato il js

--- src/frontend/pages/chat/input/insertion-bar.js
+++ src/frontend/pages/chat/input/insertion-bar.js
@@ -1,12 +1,10 @@
 import { LitElement, html, css } from "lit";
 import { when } from "lit/directives/when.js";
 
-import "./editor/editor-formatting-buttons";
 import "../../../components/button-icon";
+import "./editor/editor-formatting-buttons";
 
 import { IconNames } from "../../../enums/icon-names";
-
-import { TooltipTexts } from "../../../enums/tooltip-texts";
 
 export class InsertionBar extends LitElement {
   static properties = {
@@ -37,50 +35,64 @@ export class InsertionBar extends LitElement {
       margin-top: 0px;
       border: none;
       color: white !important;
+      font-size: 20px;
       cursor: pointer;
       display: flex;
       justify-content: center;
       align-items: center;
     }
   `;
-
+  // PER REINSERIRE L'EDITOR CAMBIARE IL METODO render CON QUELLO COMMENTATO
   render() {
     return html`
       <div>
         <div class="formatting-container">
           <il-button-icon
+            content=${IconNames.emoticon}
             @click=${this.emojiPickerClick}
-            .content=${IconNames.emoticon}
-            .tooltipText=${TooltipTexts.emoticon}
-            .condition=${true}
           ></il-button-icon>
-
-          <!-- <il-button-icon
-            @click=${() => {
-            this.bEditor = !this.bEditor;
-            this.editor?.value.focusTextarea();
-          }}
-            .content=${IconNames.pencil}
-            .tooltipText=${TooltipTexts.editor}
-          ></il-button-icon>
-          ${when(
-            this.bEditor,
-            () =>
-              html`<il-editor-formatting-buttons
-                .editor=${this.editor}
-              ></il-editor-formatting-buttons>`
-          )} -->
         </div>
         <div id="submitContainer">
           <il-button-icon
             @click=${this.sendMessage}
-            .content=${IconNames.send}
-            .tooltipText=${TooltipTexts.send}
+            content=${IconNames.send}
           ></il-button-icon>
         </div>
       </div>
     `;
   }
+  // render() {
+  //   return html`
+  //     <div>
+  //       <div class="formatting-container">
+  //         <il-button-icon
+  //           content=${IconNames.emoticon}
+  //           @click=${this.emojiPickerClick}
+  //         ></il-button-icon>
+  //         <il-button-icon
+  //           content=${IconNames.pencil}
+  //           @click=${() => {
+  //             this.bEditor = !this.bEditor;
+  //             this.editor?.value.focusTextarea();
+  //           }}
+  //         ></il-button-icon>
+  //         ${when(
+  //           this.bEditor,
+  //           () =>
+  //             html`<il-editor-formatting-buttons
+  //               .editor=${this.editor}
+  //             ></il-editor-formatting-buttons>`
+  //         )}
+  //       </div>
+  //       <div id="submitContainer">
+  //         <il-button-icon
+  //           @click=${this.sendMessage}
+  //           content=${IconNames.send}
+  //         ></il-button-icon>
+  //       </div>
+  //     </div>
+  //   `;
+  // }
 
   sendMessage() {
     this.dispatchEvent(new CustomEvent("send-message"));
