1f5f9da11da78ebfd51b902f930ecdae57d47458
Lorenzo Rinaldi <lorenzo.rinaldi@cgm.com>
2023-07-05 10:26:56 +0200

    Edited users service to add status for testing purposes

--- src/frontend/services/users-service.js
+++ src/frontend/services/users-service.js
@@ -1,17 +1,37 @@
 const axios = require("axios").default;
 
 export class UsersService {
-	static async GetUsers(query, username, password) {
-		return axios({
-			url: `/api/users?user=${query}`,
-			method: "get",
-			headers: {
-				"X-Requested-With": "XMLHttpRequest",
-			},
-			auth: {
-				username: username,
-				password: password,
-			},
-		});
-	}
+  static async GetUsers(query, username, password) {
+    // return await axios({
+    //   url: `/api/users?user=${query}`,
+    //   method: "get",
+    //   headers: {
+    //     "X-Requested-With": "XMLHttpRequest",
+    //   },
+    //   auth: {
+    //     username: username,
+    //     password: password,
+    //   },
+    // });
+
+    let users = await axios({
+      url: `/api/users?user=${query}`,
+      method: "get",
+      headers: {
+        "X-Requested-With": "XMLHttpRequest",
+      },
+      auth: {
+        username: username,
+        password: password,
+      },
+    });
+
+    users.data.forEach((_, index) => {
+      users.data[index].status = index % 2 === 0 ? "online" : "offline";
+    });
+
+    return new Promise((resolve) => {
+      resolve(users);
+    });
+  }
 }
