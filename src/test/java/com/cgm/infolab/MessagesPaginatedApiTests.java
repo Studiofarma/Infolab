bfe2d2842d7e3c25c4e14a4fd5b934b0400bcdb9
Lorenzo Rinaldi <lorenzo.rinaldi@cgm.com>
2023-07-18 10:10:20 +0200

    Test with no starting time

--- src/test/java/com/cgm/infolab/MessagesPaginatedApiTests.java
+++ src/test/java/com/cgm/infolab/MessagesPaginatedApiTests.java
@@ -183,7 +183,11 @@ public class MessagesPaginatedApiTests {
     }
 
     private String getMessageTimestampString(int messageNumber) {
-        LocalDateTime dateTime = STARTING_TIME.plusSeconds(messageNumber);
+        LocalDateTime dateTime = jdbcTemplate.queryForObject(
+                "select * from infolab.chatmessages where content = '%d. Hello general from user0'".formatted(messageNumber),
+                this::dateTimeMapper
+        );
+//        LocalDateTime dateTime = STARTING_TIME.plusSeconds(messageNumber);
         return dateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
     }
 }
