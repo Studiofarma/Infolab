9bb6231a59125c229595292a3138d9f637ad342a
Lorenzo Rinaldi <lorenzo.rinaldi@cgm.com>
2023-08-08 10:47:34 +0200

    Implemented events for prev page and for too many items in the list

--- src/frontend/components/infinite-scroll.js
+++ src/frontend/components/infinite-scroll.js
@@ -1,36 +1,78 @@
 import { LitElement, css, html } from "lit";
 import { createRef, ref } from "lit/directives/ref.js";
+import { choose } from "lit/directives/choose.js";
 
 export class InfiniteScroll extends LitElement {
-  static properties = {};
+  static properties = {
+    progressBarPos: { type: String },
+  };
 
   constructor() {
     super();
-    this.observer = null;
-    this.progressBarRef = createRef();
+    this.observerNext = null;
+    this.observerPrev = null;
+    this.progressBarNextRef = createRef();
+    this.progressBarPrevRef = createRef();
   }
 
   firstUpdated() {
     super.firstUpdated();
-    this.observer = new IntersectionObserver((entries) => {
+    this.observerNext = new IntersectionObserver((entries) => {
       if (entries[0].isIntersecting) {
-        this.dispatchEvent(new CustomEvent("update-next"));
+        this.dispatchEvent(new CustomEvent("il:update-next"));
       }
     });
 
-    this.observer.observe(this.progressBarRef.value);
+    this.observerNext.observe(this.progressBarNextRef.value);
+
+    this.observerPrev = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        this.dispatchEvent(new CustomEvent("il:update-prev"));
+      }
+    });
+
+    this.observerPrev.observe(this.progressBarPrevRef.value);
   }
 
   static styles = css``;
 
   unobserve() {
-    this.observer.unobserve(this.progressBarRef.value);
+    this.observerNext.unobserve(this.progressBarNextRef.value);
+  }
+
+  checkSlotItmesNumber(event) {
+    const slot = event.target;
+
+    if (slot.assignedElements().length > 21) {
+      // TODO: change this value (maybe make it customizable from outside).
+      this.dispatchEvent(new CustomEvent("il:update-remove"));
+    }
   }
 
   render() {
     return html`
-      <slot></slot>
-      <div id="progress-bar" ${ref(this.progressBarRef)}>Progress bar</div>
+      ${choose(
+        this.progressBarPos,
+        [
+          [
+            "top",
+            () => html`
+              <div ${ref(this.progressBarNextRef)}>Progress bar next page</div>
+              <slot @slotchange=${this.checkSlotItmesNumber}></slot>
+              <div ${ref(this.progressBarPrevRef)}>Progress bar prev</div>
+            `,
+          ],
+          [
+            "bottom",
+            () => html`
+              <div ${ref(this.progressBarPrevRef)}>Progress bar prev</div>
+              <slot @slotchange=${this.checkSlotItmesNumber}></slot>
+              <div ${ref(this.progressBarNextRef)}>Progress bar</div>
+            `,
+          ],
+        ],
+        () => html` <h1>INVALID PARAM</h1>`
+      )}
     `;
   }
 }
