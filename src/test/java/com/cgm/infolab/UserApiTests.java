9b3e6346d57c3a524f629a7dfe0719270fc47da6
McGyv246 <122812734+McGyv246@users.noreply.github.com>
2023-08-28 09:33:49 +0200

    Changed test to see if pipeline does not fail anymore

--- src/test/java/com/cgm/infolab/UserApiTests.java
+++ src/test/java/com/cgm/infolab/UserApiTests.java
@@ -43,7 +43,7 @@ public class UserApiTests {
 
     @BeforeAll
     void beforeAll() {
-        testDbHelper.clearDb();
+        testDbHelper.clearDbExceptForGeneral();
 
         Arrays.stream(users)
                 .toList()
