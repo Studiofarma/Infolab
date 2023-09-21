b670a0d153822fa5545945a8b2ba3747ef8777f4
McGyv246 <122812734+McGyv246@users.noreply.github.com>
2023-09-21 17:45:02 +0200

    Added sender description for groups notifications

--- src/frontend/pages/chat/chat.js
+++ src/frontend/pages/chat/chat.js
@@ -809,8 +809,21 @@ export class Chat extends BaseComponent {
       );
 
     if (Notification.permission === "granted") {
+      let notificationContent;
+
+      if (conversation.roomType === "GROUP") {
+        let user =
+          await this.conversationListRef.value?.getUserByUsernameOrFetch(
+            message.sender
+          );
+
+        notificationContent = `${user.description}: ${message.content}`;
+      } else {
+        notificationContent = message.content;
+      }
+
       let notification = new Notification(conversation.description, {
-        body: message.content,
+        body: notificationContent,
       });
 
       notification.onclick = async () => {
@@ -821,10 +834,23 @@ export class Chat extends BaseComponent {
         window.focus("/");
       };
     } else if (Notification.permission !== "denied") {
-      Notification.requestPermission().then(function (permission) {
+      Notification.requestPermission().then(async function (permission) {
         if (permission === "granted") {
+          let notificationContent;
+
+          if (conversation.roomType === "GROUP") {
+            let user =
+              await this.conversationListRef.value?.getUserByUsernameOrFetch(
+                message.sender
+              );
+
+            notificationContent = `${user.description}: ${message.content}`;
+          } else {
+            notificationContent = message.content;
+          }
+
           let notification = new Notification(conversation.description, {
-            body: message.content,
+            body: notificationContent,
           });
 
           notification.onclick = async () => {
