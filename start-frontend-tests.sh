ab06abfa7eebc5c848d6f05835aaac212c9dd78c
McGyv246 <122812734+McGyv246@users.noreply.github.com>
2023-09-25 17:56:19 +0200

    Implemented frontend tests in pipeline

---
+++ start-frontend-tests.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+./mvnw spring-boot:run -Dspring-boot.run.profiles=dev,inmemory &> /dev/null &
+
+endpoint="http://localhost:8081"
+max_wait_time=40
+
+start_time=$(date +%s)
+while true; do
+  echo "Waiting for backend to come online..."
+  response_code=$(curl -s -o /dev/null -w "%{http_code}" $endpoint)
+  if [ "$response_code" -eq 200 ]; then
+    echo "Connected to backend at ${endpoint}"
+    (cd "./src/frontend" && npm run cypress:norec)
+    break
+  fi
+  current_time=$(date +%s)
+  elapsed_time=$((current_time - start_time))
+  if [ $elapsed_time -ge $max_wait_time ]; then
+    echo "Could not connect to backend"
+    exit 1
+    break
+  fi
+  sleep 1
+done
